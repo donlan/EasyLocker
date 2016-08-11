07189e2d458600190d7c1b01eabda3ad5a16829f
donlan <760625325@qq.com>
2016-08-11 17:00:16 +0800

    通用回调接口

--- app/src/main/java/dong/lan/lock/utils/ActionCallback.java
+++ app/src/main/java/dong/lan/lock/utils/ActionCallback.java
@@ -4,7 +4,7 @@ package dong.lan.lock.utils;
  * Created by 梁桂栋 on 2016年08月10日 13:52.
  * Email:760625325@qq.com
  * github:gitbub.com/donlan
- * desc:
+ * desc:无返回参数的对调接口
  */
 public interface ActionCallback {
     void onActionDone();
---
+++ app/src/main/java/dong/lan/lock/utils/ActionCallback1.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package dong.lan.lock.utils;
+
+/**
+ * Created by 梁桂栋 on 2016年08月10日 13:52.
+ * Email:760625325@qq.com
+ * github:gitbub.com/donlan
+ * desc:返回一个参数的回调接口
+ */
+public interface ActionCallback1 {
+    void onActionDone(Object o);
+}
