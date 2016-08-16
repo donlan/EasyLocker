8840ba601d10babff466d006eb6d229011a4df47
donlan <760625325@qq.com>
2016-08-16 19:59:21 +0800

    加入锁屏应用跳转功能的逻辑

--- app/src/main/java/dong/lan/lock/lockAction/LockViewManager.java
+++ app/src/main/java/dong/lan/lock/lockAction/LockViewManager.java
@@ -3,27 +3,34 @@ package dong.lan.lock.lockAction;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Vibrator;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
 import java.util.Date;
 
-import dong.lan.lock.BuildConfig;
+import dong.lan.lock.Global;
 import dong.lan.lock.R;
+import dong.lan.lock.lockAction.intentAction.model.App;
 import dong.lan.lock.utils.Config;
 import dong.lan.lock.utils.ImageHelper;
 import dong.lan.lock.utils.ScreenHelper;
@@ -47,15 +54,17 @@ public class LockViewManager {
     private TextClock clock;
     private ImageView background;
     private static View lockView;
-    private WindowManager windowManager = null;
+    private WeakReference<WindowManager> windowManagerRef;
     public static LockViewManager manager = null;
     private static WindowManager.LayoutParams layoutParams;
     public static Vibrator vibrator;
     private volatile boolean isLock = false;
+    private int intentFlag = Global.FLAG_GET_UNLOCK;
 
     private String patter;
     private int lockItemColor;
     private int textColor;
+    private LockStatusListener lockStatusListener;
 
     public static synchronized LockViewManager getInstance(Activity activity) {
         if (manager == null)
@@ -69,12 +78,18 @@ public class LockViewManager {
                 .getSystemService(Context.VIBRATOR_SERVICE);
         date = new Date();
         patter = Config.getString(activity, Config.SP_LOCK_PATTER);
+        if (patter.equals(""))
+            patter = "ABCD";
         lockItemColor = Config.getInt(activity, Config.SP_LOCK_ITEM_COLOR);
+        if (lockItemColor == 0)
+            lockItemColor = Color.parseColor("#FFAE00");
         textColor = Config.getInt(activity, Config.SP_LOCK_TEXT_COLOR);
+        if (textColor == 0)
+            textColor = Color.parseColor("#34D800");
         init();
     }
 
-    public synchronized void updateActivity(Activity ac) {
+    public void updateActivity(Activity ac) {
         activity = ac;
     }
 
@@ -116,8 +131,26 @@ public class LockViewManager {
         Button resetBtn = (Button) lockView.findViewById(R.id.lock_reset);
         resetBtn.setOnClickListener(l);
         setLockItemBackground(resetBtn);
-        Button funBtn = (Button) lockView.findViewById(R.id.lock_);
-        funBtn.setOnClickListener(l);
+        CheckBox funBtn = (CheckBox) lockView.findViewById(R.id.lock_);
+        funBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                intentFlag = isChecked ? Global.FLAG_INTENT_GET : Global.FLAG_INTENT_APP;
+                if(intentFlag == Global.FLAG_INTENT_APP){
+                    App app = Global.getAppByPattern(key);
+                    if (app != null) {
+                        PackageManager pm = activity.getPackageManager();
+                        Intent launcherIntent = pm.getLaunchIntentForPackage(app.getPkgName());
+                        activity.startActivity(launcherIntent);
+                        unLock();
+                    } else {
+                        show("没有匹配的应用");
+                    }
+                    reset();
+                    intentFlag = Global.FLAG_GET_UNLOCK;
+                }
+            }
+        });
         setLockItemBackground(funBtn);
         setLockBg();
         String topStr = Config.getLockTopText(activity);
@@ -139,41 +172,54 @@ public class LockViewManager {
 
     private void setLockItemBackground(View item) {
         Drawable drawable = null;
-        if (Build.VERSION.SDK_INT >= 21)
-            drawable = ImageHelper.tintDrawable(activity.getDrawable(R.drawable.lock), ColorStateList.valueOf(lockItemColor));
-        else
-            drawable = ImageHelper.tintDrawable(activity.getResources().getDrawable(R.drawable.lock), ColorStateList.valueOf(lockItemColor));
+        drawable = ImageHelper.tintDrawable(ContextCompat.getDrawable(activity, R.drawable.lock), ColorStateList.valueOf(lockItemColor));
         if (Build.VERSION.SDK_INT >= 16)
             item.setBackground(drawable);
         else
             item.setBackgroundDrawable(drawable);
     }
 
+    private WindowManager getWindowManager() {
+        WindowManager windowManager = windowManagerRef.get();
+        if (windowManager == null) {
+            windowManager = activity.getWindowManager();
+            windowManagerRef = new WeakReference<WindowManager>(windowManager);
+        }
+        return windowManager;
+    }
+
     //初始化锁屏view的布局配置
     private void init() {
         isLock = false;
-        windowManager = activity.getWindowManager();
+        windowManagerRef = new WeakReference<WindowManager>(activity.getWindowManager());
         layoutParams = new WindowManager.LayoutParams();
         layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
         layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
         layoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
         layoutParams.flags = 1280;
+        layoutParams.windowAnimations = R.anim.zoom_enter;
     }
 
-    public synchronized void Lock() {
+    public synchronized void Lock() throws NoLockStatusListenerException {
+        if (lockStatusListener == null)
+            throw new NoLockStatusListenerException();
         if (lockView != null && !isLock) {
             if (isLock)
-                windowManager.updateViewLayout(lockView, layoutParams);
+                getWindowManager().updateViewLayout(lockView, layoutParams);
             else
-                windowManager.addView(lockView, layoutParams);
+                getWindowManager().addView(lockView, layoutParams);
             isLock = true;
+            lockStatusListener.onLocked();
         }
+        enterAnim();
     }
 
     public synchronized void unLock() {
-        if (windowManager != null && isLock) {
-            windowManager.removeView(lockView);
+        if (getWindowManager() != null && isLock) {
+            getWindowManager().removeView(lockView);
             isLock = false;
+            exitAnim();
+            lockStatusListener.onUnlock();
         }
     }
 
@@ -183,7 +229,6 @@ public class LockViewManager {
     private String getKey(boolean isShow) {
         String time = "";
         date.setTime(System.currentTimeMillis());
-
         if (date.getHours() < 10 && !isShow)
             time += "0" + date.getHours();
         else
@@ -203,47 +248,55 @@ public class LockViewManager {
     private void Check(String s) {
         key += s;
         time.append(s);
-
-        if (patter.equals("")) {
-            if (key.length() == 4) {
-                if (key.equals(getKey(false))) {
-                    activity.finish();
+        if (intentFlag == Global.FLAG_GET_UNLOCK) {
+            if (patter.equals("")) {
+                if (key.length() == 4) {
+                    if (key.equals(getKey(false))) {
+                        unLock();
+                    } else {
+                        vibrator.vibrate(500);
+                        show("密码不对　：）");
+                    }
+                    reset();
+                }
+            } else if (key.length() == patter.length()) {
+                String keyStr = getKey(false);
+                char[] k = new char[4];
+                for (int i = 0; i < 4; i++)
+                    k[i] = keyStr.charAt(i);
+                String code = "";
+                for (int i = 0; i < patter.length(); i++) {
+                    char c = patter.charAt(i);
+                    code += k[c - 65];
+                }
+                if (code.equals(key)) {
                     unLock();
                 } else {
                     vibrator.vibrate(350);
                     show("密码不对　：）");
                 }
-                time.setText("");
-                key = "";
-            }
-        } else if (key.length() == patter.length()) {
-            String keyStr = getKey(false);
-            char[] k = new char[4];
-            for (int i = 0; i < 4; i++)
-                k[i] = keyStr.charAt(i);
-            String code = "";
-            for (int i = 0; i < patter.length(); i++) {
-                char c = patter.charAt(i);
-                code += k[c - 65];
-            }
-            if (code.equals(key)) {
-                activity.finish();
-                unLock();
-            } else {
-                vibrator.vibrate(350);
-                show("密码不对　：）");
-            }
-            if (BuildConfig.DEBUG) Log.d("LockViewManager", code + "," + key);
-            time.setText("");
-            key = "";
+                reset();
 
+            }
         }
     }
 
-    private void anim(View target) {
-        vibrator.vibrate(50);
-        ObjectAnimator.ofFloat(target, "scaleX", 1f, 0.8f, 1f).setDuration(250).start();
-        ObjectAnimator.ofFloat(target, "scaleY", 1f, 0.8f, 1f).setDuration(250).start();
+    private void reset() {
+        time.setText("");
+        key = "";
+    }
+
+    public void enterAnim() {
+        ObjectAnimator.ofFloat(lockView, "scaleX", 0.5f, 0.8f, 1f).setDuration(300).start();
+        ObjectAnimator.ofFloat(lockView, "scaleY", 0.5f, 0.8f, 1f).setDuration(300).start();
+        ObjectAnimator.ofFloat(lockView, "alpha", 0.5f, 0.8f, 1f).setDuration(300).start();
+
+    }
+
+    public void exitAnim() {
+        ObjectAnimator.ofFloat(lockView, "scaleX", 1f, 0.5f, 0f).setDuration(300).start();
+        ObjectAnimator.ofFloat(lockView, "scaleY", 1f, 0.5f, 0f).setDuration(300).start();
+        ObjectAnimator.ofFloat(lockView, "alpha", 1f, 0.5f, 0f).setDuration(300).start();
     }
 
     View.OnClickListener l = new View.OnClickListener() {
@@ -253,22 +306,38 @@ public class LockViewManager {
             if (id == R.id.lock_reset) {
                 time.setText("");
                 key = "";
-            } else if (id == R.id.lock_) {
-                //// TODO: 16-8-9  
-            } else {
+            }else {
                 if (vibrator.hasVibrator()) {
                     vibrator.cancel();
                 }
                 vibrator.vibrate(20);
                 TextView textView = (TextView) v;
-                Check(textView.getText().toString());
+                if(intentFlag == Global.FLAG_INTENT_GET){
+                    IntentCheck(textView.getText().toString());
+                }else
+                    Check(textView.getText().toString());
             }
         }
     };
 
+    private void IntentCheck(String string) {
+        key+=string;
+        time.append(string);
+    }
+
 
     public void show(String str) {
-        Snackbar.make(lockView, str, 1000).show();
+        Snackbar.make(lockView, str, Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void setLockStatusListener(LockStatusListener listener) {
+        this.lockStatusListener = listener;
+    }
+
+    public interface LockStatusListener {
+        void onLocked();
+
+        void onUnlock();
     }
 
 }
